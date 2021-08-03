09e5c4706e6929b09568cd59b0b8ecb2145a4767
jkim68888 <jkim68888@gmail.com>
2021-08-03 09:29:50 +0900

    fix app.vue

--- twitter-clone/src/App.vue
+++ twitter-clone/src/App.vue
@@ -1,11 +1,9 @@
 <template>
-
+  <h1 class="text-lg bg-gray-900 text-white m-10">hello world</h1>
 </template>
 
-<script setup>
-
+<script>
+export default {};
 </script>
 
-<style>
-
-</style>
+<style></style>
